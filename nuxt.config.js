602dfeda295df03c239dae46f4584ea28a012002
AbolfazlMarzban <abolfazlmarzban39@gmail.com>
2023-06-14 16:22:03 +0330

    abol 1402-3-24

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   css: ["~assets/style.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/TiptapVuetify.js"],
+  plugins: ["~plugins/TiptapVuetify"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
